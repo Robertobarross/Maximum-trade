ca4831dc309cb1a6b3c57bc75cec3e07ed6ad4bc
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-07-05 23:13:31 -0300

    Create home.blade.php

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('template'){{--Extenção do arquivo template--}}
+
+@section('title', 'Maximum-trade'){{--extenção do título--}}
+
+@section('content'){{--Extenção do conteúdo--}}
+
+
+
+@endsection {{--Fim da extenção coteúdo--}}
