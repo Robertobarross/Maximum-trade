c82e7eee64afcee298c9eecfc2a2bb4d882e4438
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-07-05 23:13:36 -0300

    Create template.blade.php

---
+++ resources/views/template.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/script.js"></script>
+
+</head>
+<body>
+
+
+
+    {{--Início da div top-----------------------------------------------------------------------------------------------}}
+    <div id="topo">
+        <div class="topo">
+            <img src="img/logo.jpeg" class="logo" alt="">
+        </div>
+
+        <div class="menu">
+            <a href="/dashboard">Login</a> <a href="#">Cadastrar</a>
+        </div>
+    </div>
+    {{--Fim da div top--------------------------------------------------------------------------------------------------}}
+
+
+
+
+    {{--Início da div centro da página----------------------------------------------------------------------------------}}
+    <div id="centro">
+
+    </div>
+    {{--Fim da div centro da página-------------------------------------------------------------------------------------}}
+
+
+
+
+    {{--Início da div rodapé--------------------------------------------------------------------------------------------}}
+    <div id="rodape">
+        <h1 class="textos">&COPY; 2021 | E-Commerce | Maximum-trade | Todos os direitos reservados</h1>
+    </div>
+    {{--Fim da div rodapé-----------------------------------------------------------------------------------------------}}
+
+    
+
+
+</body>
+</html>
