c1a0369483015a6e9c4f62a6784eb0e25eb0d0bb
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-07-06 21:33:27 -0300

    Update forgot-password.blade.php

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,11 +1,18 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+
+            {{--Adaptações feitas na tela ------------------------------------------------------------------------------}}
+            <link rel="stylesheet" href="css/style.css">
+            <img src="img/login.png" class="img-login" alt="">
+            {{--Fim das adaptações feitas na tela ----------------------------------------------------------------------}}
+
+            {{--<x-jet-authentication-card-logo />--}}
         </x-slot>
 
         <div class="mb-4 text-sm text-gray-600">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+            {{ __('Esqueceu sua senha? Não há problema. Basta nos informar seu endereço de e-mail e enviaremos por e-mail um link de redefinição de senha que permitirá que você escolha um novo.
+            ') }}
         </div>
 
         @if (session('status'))
@@ -26,7 +33,7 @@
 
             <div class="flex items-center justify-end mt-4">
                 <x-jet-button>
-                    {{ __('Email Password Reset Link') }}
+                    {{ __('Link de redefinição de senha') }}
                 </x-jet-button>
             </div>
         </form>
