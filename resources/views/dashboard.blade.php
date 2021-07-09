802c7b220f875ef78ef9f43bec326770c4ee1d24
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-07-08 23:06:34 -0300

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,31 +1,9 @@
-{{--<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
-            </div>
-        </div>
-    </div>
-</x-app-layout>--}}
-
 @extends('template'){{--Extenção do arquivo template--}}
 
-@section('title', 'Maximum-trade'){{--extenção do título--}}
+@section('title', 'Maximum-trade'){{--Extenção do título--}}
 
-@section('content'){{--Extenção do conteúdo--}}
+@section('content'){{--Conteúdo--}}
 
+<h1>teste</h1>
 
-<h1>Seja bem vindo!</h1>
-
-
-
-@endsection {{--Fim da extenção coteúdo--}}
-
-
-
+@endsection {{--Conteúdo--}}
