e4839a3e0cf27f5561f446988f099dddbc1eede2
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-07-08 23:06:08 -0300

    Create EmpresaController.php

---
+++ app/Http/Controllers/EmpresaController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Empresa; // Incluindo Model Empresa //
+
+class EmpresaController extends Controller
+{
+    //
+}
