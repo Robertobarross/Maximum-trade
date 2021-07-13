889178cac6eab6c811ba75ea469b5c30d8bee6f5
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-07-12 22:30:23 -0300

    Update EmpresaController.php

--- app/Http/Controllers/EmpresaController.php
+++ app/Http/Controllers/EmpresaController.php
@@ -4,11 +4,46 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Empresa; // Incluindo Model Empresa //
+use App\Models\User; // Necessário para defineir o dono da Empresa //
+
 
 class EmpresaController extends Controller
 {
-  //
+    public function store(Request $request){
+
+        $empresa = new Empresa();
+
+        $empresa->empresa = $request->empresa;
+        $empresa->cpf = $request->cpf;
+        $empresa->cnpj = $request->cnpj;
+        $empresa->endereco = $request->endereco;
+        $empresa->cidade = $request->cidade;
+        $empresa->bairro = $request->bairro;
+        $empresa->uf = $request->uf;
+        $empresa->proprietario = $request->proprietario;
+        $empresa->numero = $request->numero;
+        $empresa->contratosocial = $request->contratosocial;
+        $empresa->registrocomercial = $request->registrocomercial;
+        $empresa->inscricaoestadual = $request->inscricaoestadual;
+        $empresa->alvara = $request->alvara;
+        $empresa->complemento = $request->complemento;
+
+    }
+
+   /* $User = auth()->user(); // Para separar evento por usuário //
+    $event->user_id = $user->id;
+
+
+    $empresa->save();// Para que a postagem fique gravada no banco de dados //
+
+    /* Após a postagem, o usuário será redirecionado para página criar eventos. Aparecerá a mensagem 'Evento criado com sucesso!'  */
+  /*  return redirect('/events/create')->with('msg', 'Evento criado com sucesso!');*/
+
 }
+
+
+
+
 
 
 
