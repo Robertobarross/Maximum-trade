6053ef5027c748977173969e3c1249675a4c6a74
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-07-13 22:17:06 -0300

    Update create-empresa.blade.php

--- resources/views/create-empresa.blade.php
+++ resources/views/create-empresa.blade.php
@@ -7,84 +7,52 @@
 {{--Início da div criar empresa-----------------------------------------------------------------------------------------}}
 <div id="criar-empresa">
 
-    <h1 class="titulos-centro">CADASTRE SUA LOJA</h1>
+    <p></p><h1 class="titulos-form">CADASTRE SUA LOJA</h1>
     <form action="/empresas" method="POST" enctype="multipart/form-data">
 
-            <br><label for="title" class="textos">Razão social</label>
-            <br><input type="text" class="form-control" id="empresa" name="empresa" placeholder="Nome da empresa" required>
+            <br><label for="title" class="titulos-form">Razão social</label>
+            <br><input type="text" class="form-empresa" id="empresa" name="empresa" placeholder="Nome da empresa" required>
 
-            <br><label for="title" class="textos">CPF</label>
-            <br><input type="number" class="form-control" id="cpf" name="cpf" placeholder="Informe o CPF" required>
+            <p></p><label for="title" class="titulos-form">CPF</label>
+            <br><input type="number" class="form-empresa" id="cpf" name="cpf" placeholder="Informe o CPF" required>
 
-            <br><label for="title" class="textos">CNPJ</label>
-            <br><input type="number" class="form-control" id="cnpj" name="cnpj" placeholder="CNPJ">
+            <p><label for="title" class="titulos-form">CNPJ</label>
+            <br><input type="number" class="form-empresa" id="cnpj" name="cnpj" placeholder="CNPJ">
 
-            <br><label for="title" class="textos">Endereço</label>
-            <br><input type="text" class="form-control" id="endereco" name="endereco" placeholder="Endereço da loja" required>
+            <p><label for="title" class="titulos-form">Endereço</label>
+            <br><input type="text" class="form-empresa" id="endereco" name="endereco" placeholder="Endereço da loja" required>
 
-            <br><label for="title" class="textos">Cidade</label>
-            <br><input type="text" class="form-control" id="cidade" name="cidade" placeholder="Nome da cidade" required>
+            <p><label for="title" class="titulos-form">Cidade</label>
+            <br><input type="text" class="form-empresa" id="cidade" name="cidade" placeholder="Nome da cidade" required>
 
-            <br><label for="title" class="textos">Bairro</label>
-            <br><input type="text" class="form-control" id="bairro" name="bairro" placeholder="Informe o bairro" required>
+            <p><label for="title" class="titulos-form">Bairro</label>
+            <br><input type="text" class="form-empresa" id="bairro" name="bairro" placeholder="Informe o bairro" required>
 
-          {{--  <br><label for="title" class="textos">UF</label>
-            <br><input type="text" class="form-control" id="uf" name="uf" placeholder="Estado, ex: SP" required> --}}
+            <p><label for="title" class="titulos-form">UF</label>
+            <br><input type="text" class="form-empresa" id="uf" name="uf" placeholder="Informe o estado" required>
 
-            <br><label for="title" class="textos">UF</label>
-            <br><select name="uf" input type="text" class="#" id="uf" name="uf" required>
-                <option value="estado">Selecione o Estado</option>
-                <option value="ac">Acre</option>
-                <option value="al">Alagoas</option>
-                <option value="am">Amazonas</option>
-                <option value="ap">Amapá</option>
-                <option value="ba">Bahia</option>
-                <option value="ce">Ceará</option>
-                <option value="df">Distrito Federal</option>
-                <option value="es">Espírito Santo</option>
-                <option value="go">Goiás</option>
-                <option value="ma">Maranhão</option>
-                <option value="mt">Mato Grosso</option>
-                <option value="ms">Mato Grosso do Sul</option>
-                <option value="mg">Minas Gerais</option>
-                <option value="pa">Pará</option>
-                <option value="pb">Paraíba</option>
-                <option value="pr">Paraná</option>
-                <option value="pe">Pernambuco</option>
-                <option value="pi">Piauí</option>
-                <option value="rj">Rio de Janeiro</option>
-                <option value="rn">Rio Grande do Norte</option>
-                <option value="ro">Rondônia</option>
-                <option value="rs">Rio Grande do Sul</option>
-                <option value="rr">Roraima</option>
-                <option value="sc">Santa Catarina</option>
-                <option value="se">Sergipe</option>
-                <option value="sp">São Paulo</option>
-                <option value="to">Tocantins</option>
-            </select>
+            <p><label for="title" class="titulos-form">Proprietário</label>
+            <br><input type="text" class="form-empresa" id="proprietario" name="proprietario" placeholder="Informe o nome do proprietário" required>
 
-            <br><label for="title" class="textos">Evento</label>
-            <br><input type="text" class="form-control" id="titulo" name="titulo" placeholder="Nome do evento" require>
+            <p><label for="title" class="titulos-form">Número</label>
+            <br><input type="number" class="form-empresa" id="numero" name="numero" placeholder="Inserir número" required>
 
-            <br><label for="title" class="textos">Evento</label>
-            <br><input type="text" class="form-control" id="titulo" name="titulo" placeholder="Nome do evento" require>
+            <p><label for="title" class="titulos-form">Contrato Social</label>
+            <br><input type="text" class="form-empresa" id="contratosocial" name="contratosocial" placeholder="Insira o número do contrato">
 
-            <br><label for="title" class="textos">Evento</label>
-            <br><input type="text" class="form-control" id="titulo" name="titulo" placeholder="Nome do evento">
+            <p><label for="title" class="titulos-form">Registro Comercial</label>
+            <br><input type="text" class="form-empresa" id="registrocomercial" name="registrocomercial" placeholder="Registro da junta comercial">
 
-            <br><label for="title" class="textos">Evento</label>
-            <br><input type="text" class="form-control" id="titulo" name="titulo" placeholder="Nome do evento">
+            <p><label for="title" class="titulos-form">Inscrição Estadual</label>
+            <br><input type="text" class="form-empresa" id="inscricaoestadual" name="inscricaoestadual" placeholder="Número da inscrição estadual">
 
-            <br><label for="title" class="textos">Evento</label>
-            <br><input type="text" class="form-control" id="titulo" name="titulo" placeholder="Nome do evento">
+            <p><label for="title" class="titulos-form">Alvará</label>
+            <br><input type="text" class="form-empresa" id="alvara" name="alvara" placeholder="Número do alvará">
 
-            <br><label for="title" class="textos">Evento</label>
-            <br><input type="text" class="form-control" id="titulo" name="titulo" placeholder="Nome do evento">
+            <p><label for="title" class="titulos-form">Complemento</label>
+            <br><textarea name="complemento" id="complemento" cols="30" rows="10" class="form-empresa" placeholder="Informações adicionais..."></textarea>
 
-            <br><label for="title" class="textos">Evento</label>
-            <br><input type="text" class="form-control" id="titulo" name="titulo" placeholder="Nome do evento">
-
-            <input type="Submit" class="btn btn-primary" value="Criar Evento">
+            <p><input type="Submit" class="btn-btn" value="CADASTRAR"></p>
 
     </form>
 
