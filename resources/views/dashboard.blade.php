e1abf04a1169e3c4c89c270fa0c751cb3babd0da
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-07-16 21:26:53 -0300

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,5 +4,17 @@
 
 @section('content'){{--Conteúdo--}}
 
+<h1>teste</h1>
+
+if(count($empresas) > 0)
+foreach (@empresas as @empresa)
+
+{{ $empresa->empresa }}
+
+endforeach
+
+else{{--Se o usuário não tiver nem uma empresa cadastrada, será emitida a mensagem a baixo--}}
+<p>Você ainda não tem empresa cadastrada, <a href="/inserir">Inserir horas</a></p>
+endif
 
 @endsection {{--Conteúdo--}}
