6348718035d38cf51c2da2667a5849a330e403dd
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-09-09 22:39:16 -0300

    Update Empresa.php

--- app/Models/Empresa.php
+++ app/Models/Empresa.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Empresa extends Model
 {
     use HasFactory;
+    // Para autenticação do usuário
+    public function user() {
+        return $this->belongsTo('App\Models\User');
+    }
 }
