d3ecb83698b1547d070b2d04625b3826cf3989f5
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-09-09 22:39:38 -0300

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,6 +4,7 @@
 
 @section('content'){{--Conteúdo--}}
 
+
 @if(count($empresas) > 0)
 @foreach ($empresas as $empresa)
 
@@ -11,16 +12,15 @@
 
 @endforeach
 
-@else{{--Se o usuário não tiver loja cadastrada, será emitida a mensagem a baixo--}}
+@else {{--Se o usuário não tiver loja cadastrada, será emitida a mensagem a baixo--}}
 <p>Você ainda não tem loja cadstrada, <a href="/create-empresa">Cadastrar Loja</a></p>
 @endif
 
 @auth
-    {{--Link para acessar empresa do usuário que está logado, está vinculado a rota show--}}
-<br><a href="/empresas/{{ $empresa->user_id }}" class="links-dashboard">Acessar minha loja</a>
+
+   {{--Link para acessar empresa do usuário que está logado, está vinculado a rota show--}}
+   <br><a href="/empresas/{{ $empresa->user_id }}" class="links-dashboard">Acessar minha loja</a>
 
 @endauth
-
-
 
 @endsection {{--Conteúdo--}}
