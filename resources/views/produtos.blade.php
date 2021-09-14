05d2411c018c44988ccce8aee8d3ac8fbbeedcb5
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-09-13 23:31:14 -0300

    Create produtos.blade.php

---
+++ resources/views/produtos.blade.php
@@ -0,0 +1,9 @@
+@extends('template'){{--Extenção do arquivo template--}}
+
+@section('title', 'Meus produtos'){{--Extenção do título--}}
+
+@section('content'){{--Conteúdo--}}
+
+<h1>Meus produtos!</h1>
+
+@endsection {{--Conteúdo--}}
