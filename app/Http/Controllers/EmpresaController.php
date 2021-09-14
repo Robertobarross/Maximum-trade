d11c30210b21d81bce1ee89296b39f68cee68e76
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-09-13 23:26:23 -0300

    Update EmpresaController.php

--- app/Http/Controllers/EmpresaController.php
+++ app/Http/Controllers/EmpresaController.php
@@ -9,14 +9,14 @@ use App\Models\User; // Necessário para defineir o dono da Empresa //
 
 class EmpresaController extends Controller
 {
-    /*-----------------------------------------------------------------------------------------------------------------*/
+    /*--------------------------------*/
     public function create(){ // Rota da página criar eventos //
         return view('create-empresa');
     }
-    /*-----------------------------------------------------------------------------------------------------------------*/
+    /*--------------------------------*/
 
 
-    /*-----------------------------------------------------------------------------------------------------------------*/
+    /*------------------------------- */
     public function store(Request $request){ // Para inserir informação do formulário no Banco de dados //
 
         $empresa = new Empresa;
@@ -43,38 +43,59 @@ class EmpresaController extends Controller
         $empresa->save();// salva os arquivos no banco //
         return redirect('/create-empresa')->with('msg', 'Loja cadastradas com sucesso!');// Retornar para página criar empresa //
     }
-/*-----------------------------------------------------------------------------------------------------------------*/
+/*------------------------------*/
 
 
-/*-----------------------------------------------------------------------------------------------------------------*/
+/*------------------------------*/
     public function dashboard(){ // Permite que as informações sobre a empresa apareçam na view dashboard //
+
+      //  $empresa = new Empresa;
+
+        $empresas = Empresa::all()
+
+        ->where('user_id', auth()->user()->id); // Permite aparecer apenas dados do usuário logado
+
+        $user = auth()->user(); // Separar empresa por usuário //
+        $empresas->user_id = $user->id; // Buscar usuário //
+
+        $empresaOwner = User::where('id', '=', $empresas->user_id)->first()->toArray();
+
+    return view('dashboard', ['empresas' => $empresas, 'empresaOwner' => $empresaOwner ]);
+    }
+/*-------------------------------*/
+
+
+/*-------------------------------*/
+    public function show($id){ // Buscar loja do usuário que está logado //
+
         $empresa = new Empresa;
 
         $empresas = Empresa::all();
 
-        $user = auth()->user(); // Separar empresa por usuário //
-        $empresa->user = $user->id; // Buscar usuário //
-
-        $empresaOwner = User::where('id', $empresa->user)->first()->toArray();
-
-        return view('dashboard', ['empresas' => $empresas, 'empresaOwner' => $empresaOwner]);
-    }
-/*-----------------------------------------------------------------------------------------------------------------*/
-
-
-/*-----------------------------------------------------------------------------------------------------------------*/
-    public function show($id){ // Buscar loja do usuário que está logado //
-
         $empresa = Empresa::findOrFail($id);
 
         $user = auth()->user(); // Separar empresa por usuário //
-        $empresa->user = $user->id; // Buscar usuário //
+        $empresa->user_id = $user->id; // Buscar usuário //
 
-        $empresaOwner = User::where('id', $empresa->user)->first()->toArray();
+        $empresaOwner = User::where('id', '=', $empresa->user_id)->first()->toArray();
 
-        return view('show', ['empresa' => $empresa, 'empresaOwner' => $empresaOwner]);
+    return view('show', ['empresa' => $empresa, 'empresaOwner' => $empresaOwner]);
     }
-/*-----------------------------------------------------------------------------------------------------------------*/
+/*--------------------------------*/
+
+
+/* ------------------------------ */
+    public function cadprod(){ // Rota cadastrar produtos
+        return view('cadprod');
+    }
+/* ----------------------------- */
+
+
+/* ----------------------------- */
+    public function produtos(){
+        return view('produtos');
+    }
+/* ----------------------------- */
 
 }
 
