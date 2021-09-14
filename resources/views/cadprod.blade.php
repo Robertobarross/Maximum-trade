7bbecdbe7b791c65de7237b58d1156228111e499
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-09-13 23:31:02 -0300

    Create cadprod.blade.php

---
+++ resources/views/cadprod.blade.php
@@ -0,0 +1,10 @@
+@extends('template'){{--Extenção do arquivo template--}}
+
+@section('title', 'Cadastrar produtos'){{--Extenção do título--}}
+
+@section('content'){{--Conteúdo--}}
+
+<h1>Cadastrar produtos!</h1>
+
+
+@endsection {{--Conteúdo--}}
