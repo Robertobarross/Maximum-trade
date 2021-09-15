2f9f4b52103565a3019b71e582bdee199d3f4203
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-09-14 23:49:47 -0300

    Create Produto.php

---
+++ app/Models/Produto.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Produto extends Model
+{
+    use HasFactory;
+    // Para autenticação do usuário
+    public function user() {
+        return $this->belongsTo('App\Models\User');
+}
+}
