a105bcc559d0fe3adfbf77c6ce2a4c13cd92f799
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-09-14 23:50:20 -0300

    Create 2021_09_15_002140_create_produtos_table.php

---
+++ database/migrations/2021_09_15_002140_create_produtos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\Produto;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdutosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->text("produto")->unique();
+            $table->file("foto");
+            $table->text("informe");
+            $table->text("valor");
+            $table->date("data");
+            $table->text("obs");
+            $table->text("user_id");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+}
