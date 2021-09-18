f1419b6ade83623facbf5ed3985139dc60f7cc2c
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-09-17 23:36:37 -0300

    Update cadprod.blade.php

--- resources/views/cadprod.blade.php
+++ resources/views/cadprod.blade.php
@@ -10,12 +10,12 @@
     <p class="texto-form">{{ session('msg') }}</p>
     @endif
 
-    <form action="/cadprod" method="post" enctype="multipart/form-data"></form>
+    <form action="/produtos" method="post" enctype="multipart/form-data">
     @csrf
 
     <h1 class="form-produto">Cadastrar produto</h1>
 
-    <input type="text" id="produto" name="produto" placeholder="Nome do produto!" class="form-produto" required>
+    <input type="text" id="prod" name="prod" placeholder="Nome do produto!" class="form-produto" required>
 
     <input type="file" id="foto" name="foto" placeholder="Foto do produto!" class="form-produto" required>
 
@@ -23,11 +23,13 @@
 
     <input type="text" id="valor" name="valor" placeholder="Valor do produto!" class="form-produto" required>
 
-    <input type="date" id="data" name="data" placeholder="Data do cadastro!" class="form-produto" required>
+    <input type="date" id="dt" name="dt" placeholder="Data do cadastro!" class="form-produto" required>
 
     <input type="text" id="obs" name="obs" maxlength="100" placeholder="Observações sobre o produto!" class="form-produto" required>
 
     <p><input type="submit" class="btn-btn" value="Cadastrar"></p>
+
+</form>
 
 </div>
 
