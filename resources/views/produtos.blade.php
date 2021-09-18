6b617fcf511c2cde53177e23db045e11fe6f732b
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-09-17 23:36:41 -0300

    Update produtos.blade.php

--- resources/views/produtos.blade.php
+++ resources/views/produtos.blade.php
@@ -4,6 +4,31 @@
 
 @section('content'){{--Conteúdo--}}
 
-<h1>Meus produtos!</h1>
+<div id="produtos">
+    @csrf
+    @auth
+    @if(count($produtos) > 0)
+    @foreach ($produtos as $produto)
+    <div id="post-produtos" class="col-md-12">
+        <p class="camp-produtos">Usuário(a): {{ $produtoOwner['name'] }} </p>
+        <h1 class="camp-produtos">Produto: {{ $produto->prod }}</h1>
+        <img src="/pasta-img/img/{{ $produto->foto }}" alt="{{ $produto->foto }}" class="img-produtos">
+        <h1 class="camp-produtos">Especificações: {{ $produto->informe }}</h1>
+        <h2 class="camp-produtos">Valor R$: {{ $produto->valor }}</h2>
+        <h3 class="camp-produtos">Data da postagem: {{ date('d/m/y', strtotime($produto->dt)) }}</h3>
+        <h4 class="camp-produtos">Obs: {{ $produto->obs }}</h4>
+        <h5 class="camp-produtos">Código do produto: {{ $produto->id }}</h5>
+
+        <input type="Submit" class="btn-produtos" value="Editar">
+    </div>
+
+   @endforeach
+   @else
+   <p>Você não tem produtos cadastrados! <a href="/cadprod">Cadastre sue produto!</a> </p>
+   @endif
+   @endauth
+
+</div>
+
 
 @endsection {{--Conteúdo--}}
