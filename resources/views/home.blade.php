fe1960ca14618d06d146247b7ed48611a52f2f0d
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-09-27 22:14:58 -0300

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,14 +17,14 @@
 @foreach ($produtos as $produto)
 <div id="produtos-home" align="center">
     <h1 class="prod-home">Produto: {{ $produto->prod }}</h1>
-    <img src="/pasta-img/img/{{ $produto->foto }}" alt="{{ $produto->foto }}" class="img-produtos">
+    <img src="/pasta-img/img/{{ $produto->foto }}" alt="{{ $produto->foto }}" class="img-produtos-home">
     <h1 class="prod-home">Especificações: {{ $produto->informe }}</h1>
     <h2 class="prod-home">Valor R$: {{ $produto->valor }}</h2>
     <h3 class="prod-home">Data da postagem: {{ date('d/m/y', strtotime($produto->dt)) }}</h3>
     <h4 class="prod-home">Obs: {{ $produto->obs }}</h4>
     <h5 class="prod-home">Código do produto: {{ $produto->id }}</h5>
 
-    <a href="/produtos/{{ $produto->id }}"><button class="btn-produtos">Comprar</button></a>
+    <a href="/produtos/carrinho/{{ $produto->id }}"><button class="btn-produtos">Comprar</button></a>
 </div>
 @endforeach
 @endif
