894868af2a7ad3f026342dd29f7cf604ca5a1a38
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-09-27 22:15:03 -0300

    Update produtos.blade.php

--- resources/views/produtos.blade.php
+++ resources/views/produtos.blade.php
@@ -10,21 +10,15 @@
     <p class="texto-form">{{ session('msg') }}</p>
     @endif
 
+    <p class="camp-produtos">Usuário(a): {{ $produtoOwner['name'] }} </p>
     @csrf
     @auth
     @if(count($produtos) > 0)
     @foreach ($produtos as $produto)
     <div id="post-produtos">
-        <p class="camp-produtos">Usuário(a): {{ $produtoOwner['name'] }} </p>
-        <h1 class="camp-produtos">Produto: {{ $produto->prod }}</h1>
+        <p></p><h1 class="camp-produtos">Código do produto: {{ $produto->id }} - Produto: {{ $produto->prod }} - Valor R$: {{ $produto->valor }} - Data da postagem: {{ date('d/m/y', strtotime($produto->dt)) }} -
         <img src="/pasta-img/img/{{ $produto->foto }}" alt="{{ $produto->foto }}" class="img-produtos">
-        <h1 class="camp-produtos">Especificações: {{ $produto->informe }}</h1>
-        <h2 class="camp-produtos">Valor R$: {{ $produto->valor }}</h2>
-        <h3 class="camp-produtos">Data da postagem: {{ date('d/m/y', strtotime($produto->dt)) }}</h3>
-        <h4 class="camp-produtos">Obs: {{ $produto->obs }}</h4>
-        <h5 class="camp-produtos">Código do produto: {{ $produto->id }}</h5>
-
-        <a href="/produtos/edit/{{ $produto->id }}"><button class="btn-produtos">Editar</button></a>
+        <a href="/produtos/edit/{{ $produto->id }}"><button class="btn-produtos">Editar</button></a></p>
     </div>
 
    @endforeach
