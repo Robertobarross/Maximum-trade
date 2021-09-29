0de66c6c458eabb6d40dad962a58aa97b8c0b2ce
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-09-28 23:39:59 -0300

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,29 +12,43 @@
 
 {{--Aqui na home estão disponíveis todos os produtos--}}
 <div id="home-produtos">
+    
+@if (session('msg')){{--Mensagem de atualização do produto--}}
+    <p class="texto-form">{{ session('msg') }}</p>
+    @endif
 
-{{--Criei um form com os dados do produto para fazer a ponte entre o bd e o carrinho--}}
-<form action="/produtos" method="post" enctype="multipart/form-data">
-@csrf
 
 @if(count($produtos) > 0)
 @foreach ($produtos as $produto)
 <div id="produtos-home" align="center">
-    <h1 class="prod-home">Produto: {{ $produto->prod }}</h1>
-    <img src="/pasta-img/img/{{ $produto->foto }}" alt="{{ $produto->foto }}" class="img-produtos-home">
-    <h1 class="prod-home">Especificações: {{ $produto->informe }}</h1>
-    <h2 class="prod-home">Valor R$: {{ $produto->valor }}</h2>
-    <h3 class="prod-home">Data da postagem: {{ date('d/m/y', strtotime($produto->dt)) }}</h3>
-    <h4 class="prod-home">Obs: {{ $produto->obs }}</h4>
-    <h5 class="prod-home">Código do produto: {{ $produto->id }}</h5>
 
-    <a href="/produtos/carrinho/{{ $produto->id }}"><button class="btn-produtos">Comprar</button></a>
+    {{--Criei um form com os dados do produto para fazer a ponte entre o bd e o carrinho--}}
+    <img src="/pasta-img/img/{{ $produto->foto }}" alt="{{ $produto->foto }}" class="img-produtos-home">
+<form action="/carrinhos" method="post" enctype="multipart/form-data">
+    @csrf
+
+    <h1 class="prod-home">Produto:</h1>
+    <br><input type="Text" id="prod" name="prod" class="prod-home" value="<?php echo  $produto->prod ?>" readonly/>
+
+    <h2 class="prod-home">Especificações:</h2>
+    <br><input type="Text" id="informe" name="informe" class="prod-home" value="<?php echo  $produto->informe ?>" readonly/>
+
+    <h3 class="prod-home">Valor:</h3>
+    <br><input type="text" id="valor" name="valor" class="prod-home" value="<?php echo $produto->valor ?>" readonly/>
+
+    <h4 class="prod-home">Data da postagem:</h4>
+    <br><input type="text" id="dt" name="dt" class="prod-home" value="<?php echo $produto->dt ?>" readonly/>
+
+    <h5 class="prod-home">Observações:</h5>
+    <br><input type="text" id="obs" name="obs" class="prod-home" value="<?php echo  $produto->obs ?>" readonly/>
+
+    <br><button class="btn-produtos">Comprar</button>
+
+
+</form>
 </div>
 @endforeach
 @endif
-
-</form>
-
 </div>
 
 @endsection {{--Conteúdo--}}
