3b2ea983144bba0de378191c7f27fd414347e693
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-09-28 23:39:21 -0300

    Create 2021_09_28_215650_create_carrinhos_table.php

---
+++ database/migrations/2021_09_28_215650_create_carrinhos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarrinhosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carrinhos', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->text("prod")->unique();
+            $table->file("foto");
+            $table->text("informe");
+            $table->text("valor");
+            $table->date("dt");
+            $table->text("obs");
+            $table->text("user_id");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carrinhos');
+    }
+}
