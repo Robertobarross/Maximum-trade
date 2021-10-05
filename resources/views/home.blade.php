4517a2b1019c9eef695e0eeb1f751bcf101bdb90
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-10-04 23:52:21 -0300

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,7 @@
 
 {{--Aqui na home estão disponíveis todos os produtos--}}
 <div id="home-produtos">
-    
+
 @if (session('msg')){{--Mensagem de atualização do produto--}}
     <p class="texto-form">{{ session('msg') }}</p>
     @endif
@@ -27,22 +27,26 @@
 <form action="/carrinhos" method="post" enctype="multipart/form-data">
     @csrf
 
-    <h1 class="prod-home">Produto:</h1>
-    <br><input type="Text" id="prod" name="prod" class="prod-home" value="<?php echo  $produto->prod ?>" readonly/>
+    <h1 class="prod-home-titulos">Produto:</h1>
+    <input type="Text" id="prod" name="prod" class="prod-home" value="<?php echo  $produto->prod ?>" readonly/>
 
-    <h2 class="prod-home">Especificações:</h2>
-    <br><input type="Text" id="informe" name="informe" class="prod-home" value="<?php echo  $produto->informe ?>" readonly/>
+    <h2 class="prod-home-titulos">Especificações:</h2>
+    <h2 class="prod-home">{{ $produto->informe }}<h2>
+    {{--<input type="Text" id="informe" name="informe" class="prod-home" value="<?php echo  $produto->informe ?>" readonly/>--}}
 
-    <h3 class="prod-home">Valor:</h3>
-    <br><input type="text" id="valor" name="valor" class="prod-home" value="<?php echo $produto->valor ?>" readonly/>
+    <h3 class="prod-home-titulos">Valor:</h3>
+    <input type="text" id="valor" name="valor" class="prod-home" value="<?php echo $produto->valor ?>" readonly/>
 
-    <h4 class="prod-home">Data da postagem:</h4>
-    <br><input type="text" id="dt" name="dt" class="prod-home" value="<?php echo $produto->dt ?>" readonly/>
+    <h4 class="prod-home-titulos">Data da postagem:</h4>
+    <input type="text" id="dt" name="dt" class="prod-home" value="<?php echo $produto->dt ?>" readonly/>
 
-    <h5 class="prod-home">Observações:</h5>
-    <br><input type="text" id="obs" name="obs" class="prod-home" value="<?php echo  $produto->obs ?>" readonly/>
+    <h5 class="prod-home-titulos">Observações:</h5>
+    <input type="text" id="obs" name="obs" class="prod-home" value="<?php echo  $produto->obs ?>" readonly/>
 
-    <br><button class="btn-produtos">Comprar</button>
+    <h5 class="prod-home-titulos">Código do produto:</h5>
+    <input type="text" id="idprod" name="idprod" class="prod-home" value="<?php echo  $produto->id ?>" readonly/>
+
+    <br><button class="btn-produtos">Adicionar ao carrinho</button>
 
 
 </form>
